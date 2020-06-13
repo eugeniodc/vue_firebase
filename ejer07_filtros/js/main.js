d4b599146c5429358e29d1b922a4c42b3815bea0
eugeniodc <diaz.eugenio@gmail.com>
2020-06-13 17:56:45 +0200

    Filtros

---
+++ ejer07_filtros/js/main.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: 'main',
+    data: {
+        busqueda: '',
+        minimo: 5,
+        juegos: [{
+            titulo: 'Battlefield I',
+            genero: 'FPS',
+            puntuacion: 9
+        },
+        {
+            titulo: ' Civilization VI',
+            genero: 'Estrategia',
+            puntuacion: 10
+        },
+        {
+            titulo: 'Resident Evil 7',
+            genero: 'Survival Horror',
+            puntuacion: 7,
+        }
+        ]
+    },
+computed: {
+    mejoresJuegos(){
+        return this.juegos.filter((juego)=>juego.puntuacion >=  this.minimo );
+    },
+    buscarJuegos() {
+        return this.juegos.filter((juego)=>juego.titulo.includes(this.busqueda));
+    }
+}
+});
