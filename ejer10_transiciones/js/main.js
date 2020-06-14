5263d380f83af37532d174f7c0c1b7554907729a
eugeniodc <diaz.eugenio@gmail.com>
2020-06-14 18:29:44 +0200

    Transiciones I parte

---
+++ ejer10_transiciones/js/main.js
@@ -0,0 +1,13 @@
+
+new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensajes: {
+            transicion: 'Transicion CSS con Vue.js',
+            animacion: 'Animaciones CSS con Vue.js',
+            animationCustom: 'Animaciones custom CSS con Vue.js',
+            entreElementos: 'Transiciones entre elementos con Vu.js',
+        }
+    },
+});
