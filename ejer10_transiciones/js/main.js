d5bfe442fa83a3562376a630c37130f7a2c3e6a1
eugeniodc <diaz.eugenio@gmail.com>
2020-06-14 19:55:05 +0200

    transiciones y animaciones

--- ejer10_transiciones/js/main.js
+++ ejer10_transiciones/js/main.js
@@ -1,3 +1,7 @@
+/* Vue permite realizar todo tipo de transiciones y animaciones
+(transition, animation y JavaScript animation) a los elementos de tu vista,
+utilizando el componente transition y una serie de prefijos de nombre de clase predefinidos.
+Si además, quieres utilizar una colección de animaciones CSS como Animate.css, nunca lo has tenido tan fácil. */
 
 new Vue({
     el: 'main',
