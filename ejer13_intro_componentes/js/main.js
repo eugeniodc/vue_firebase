47476f50ec61d5f0133f2b38ef0fb505224eaef5
eugeniodc <diaz.eugenio@gmail.com>
2020-06-21 11:15:12 +0200

    Componentes Intro - 2

--- ejer13_intro_componentes/js/main.js
+++ ejer13_intro_componentes/js/main.js
@@ -1,33 +1,23 @@
 Vue.component('mis-tareas', {
-    props: ['tareas'],
     template: '<ul><li v-for="tarea in tareas">{{ tarea.title }}</li></ul>',
-})
-
-// Vue.component('blog-post', {
-//     // formato camelCase en JavaScript
-//     props: ['postTitle'],
-//     template: '<h3>{{ postTitle }}</h3>'
-//   })
-
-new Vue({
-    el: 'main',
     mounted() {
         axios.get('http://jsonplaceholder.typicode.com/todos')
             .then((respuesta) => {
                 console.log(respuesta);
-                this.tareasAjax = respuesta.data;
+                this.tareas = respuesta.data;
             });
 
     },
-    data: {
-        tareasAjax: [],
-        tareasLocales: [
-            { title: 'Hacer la compra' },
-            { title: 'Aprender Vue' },
-            { title: 'Aprender Javascript ES6' },
-        ]
+    data() {
+        return {
+            tareas: [],
+        }
     }
+});
 
+
+new Vue({
+    el: 'main',
 });
 
 
